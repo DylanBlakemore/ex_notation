baecb23a083f3a6011b3c9c7fce8fb336613da35
Dylan Blakemore <dylan.blakemore@gmail.com>
2024-01-05 18:34:39 +0200

    Add bumpit

--- mix.exs
+++ mix.exs
@@ -7,22 +7,37 @@ defmodule ExNotation.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.31.0"},
+      {:bumpit, "~> 0.1.0"}
+    ]
+  end
+
+  defp description do
+    """
+    A library for parsing and evaluating mathematical expressions in Elixir using various notations
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Dylan Blakemore"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => ""}
     ]
   end
 end
