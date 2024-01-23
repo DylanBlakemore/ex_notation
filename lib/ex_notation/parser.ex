4f45fb1476a7daccb3ddb34d57d990fb68521f23
Dylan Blakemore <dylan.blakemore@gmail.com>
2024-01-23 22:47:51 +0200

    Postfix to s_expression

--- lib/ex_notation/parser.ex
+++ lib/ex_notation/parser.ex
@@ -1,8 +1,17 @@
 defmodule ExNotation.Parser do
   alias ExNotation.{Tokeniser, ShuntingYard}
 
+  @operators ["+", "-", "*", "/"]
+
   @doc """
   Converts an infix expression to postfix notation.
+
+  ## Examples
+
+      iex> ExNotation.Parser.infix_to_postfix(["1", "+", "2"])
+      ["1", "2", "+"]
+      iex> ExNotation.Parser.infix_to_postfix(["1", "+", "2", "*", "3"])
+      ["1", "2", "3", "*", "+"]
   """
   @spec infix_to_postfix(String.t() | list(String.t())) :: String.t() | list(String.t())
   def infix_to_postfix(tokens) when is_list(tokens) do
@@ -16,4 +25,31 @@ defmodule ExNotation.Parser do
     |> infix_to_postfix()
     |> Enum.join(" ")
   end
+
+  @doc """
+  Converts a postfix expression to an s-expression.
+
+  ## Examples
+
+      iex> ExNotation.Parser.postfix_to_s_expression(["1", "2", "+"])
+      {"+", {"1", "2"}}
+      iex> ExNotation.Parser.postfix_to_s_expression(["1", "2", "+", "3", "*"])
+      {"*", {{"+", {"1", "2"}}, "3"}}
+      iex> ExNotation.Parser.postfix_to_s_expression(["1", "2", "+", "3", "*", "4", "+"])
+      {"+", {{"*", {{"+", {"1", "2"}}, "3"}}, "4"}}
+  """
+  @spec postfix_to_s_expression(String.t() | list(String.t())) :: tuple()
+  def postfix_to_s_expression(tokens) do
+    tokens
+    |> Enum.reduce([], &handle_token/2)
+    |> List.first()
+  end
+
+  defp handle_token(token, stack) when token in @operators do
+    [right_op, left_op | rest] = stack
+    [{token, {left_op, right_op}} | rest]
+  end
+
+  defp handle_token(token, stack),
+    do: [token | stack]
 end
--- test/ex_notation/parser_test.exs
+++ test/ex_notation/parser_test.exs
@@ -28,4 +28,20 @@ defmodule ExNotation.ParserTest do
              ]
     end
   end
+
+  describe "postfix_to_s_expression/1" do
+    test "converts a postfix expression to an s-expression" do
+      assert Parser.postfix_to_s_expression(["1", "2", "+"]) == {"+", {"1", "2"}}
+
+      assert Parser.postfix_to_s_expression(["1", "2", "+", "3", "*"]) == {
+               "*",
+               {{"+", {"1", "2"}}, "3"}
+             }
+
+      assert Parser.postfix_to_s_expression(["1", "2", "+", "3", "*", "4", "+"]) == {
+               "+",
+               {{"*", {{"+", {"1", "2"}}, "3"}}, "4"}
+             }
+    end
+  end
 end
